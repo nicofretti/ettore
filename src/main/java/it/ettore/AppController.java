343bc5d8118dbedbb9677558e7ce6b4d128a222a
Elia Perantoni <perantonielia0@gmail.com>
2023-01-07 19:19:18 +0100

    Prepare register web page

--- src/main/java/it/ettore/AppController.java
+++ src/main/java/it/ettore/AppController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AppController {
@@ -15,12 +16,24 @@ public class AppController {
     private CourseRepository repoCourse;
 
     @GetMapping("/login")
-    public String login() {
+    public String loginPage() {
         return "login";
     }
 
+    @PostMapping("/login")
+    public String login() {
+        // TODO Implement
+        return "index";
+    }
+
     @GetMapping("/register")
-    public String register() {
+    public String registerPage() {
         return "register";
+    }
+
+    @PostMapping("/register")
+    public String register() {
+        // TODO Implement
+        return "index";
     }
 }
