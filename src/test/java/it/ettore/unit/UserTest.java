ff8ca20c9335603b2e411fe1b6826c842c3b673f
Nicolò Fretti <34170904+nicofretti@users.noreply.github.com>
2023-01-09 18:17:12 +0100

    Update UserTest.java

--- src/test/java/it/ettore/unit/UserTest.java
+++ src/test/java/it/ettore/unit/UserTest.java
@@ -7,8 +7,6 @@ import org.junit.Test;
 
 
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 public class UserTest {
     static User user;
