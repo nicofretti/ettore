2bfbfc016e4e3ce82e6402051502aeaac3ab93b8
Ledjo Lleshaj <ledjolleshaj@yahoo.com>
2023-01-10 15:57:44 +0100

    Added LoginPage object with web elements/getters/setters

---
+++ src/test/java/it/ettore/e2e/PageObjects/LoginPage.java
@@ -0,0 +1,49 @@
+package it.ettore.e2e.PageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.WebElement;
+
+public class LoginPage extends PageObject{
+
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+
+    @FindBy(tagName = "title")
+    private WebElement title;
+
+    @FindBy(name = "email")
+    private WebElement email;
+
+    @FindBy(name = "password")
+    private WebElement password;
+
+    @FindBy(name = "login")
+    private WebElement loginButton;
+
+    public String getTitle() {
+        return title.getText();
+    }
+
+    public void setEmail(String email) {
+        this.email.clear();
+        this.email.sendKeys(email);
+    }
+
+    public void setPassword(String password) {
+        this.password.clear();
+        this.password.sendKeys(password);
+    }
+
+    public CoursesPage login() {
+        loginButton.click();
+        return new CoursesPage(driver);
+    }
+
+    public RegisterPage register() {
+        driver.findElement(By.name("register")).click();
+        return new RegisterPage(driver);
+    }
+
+}
