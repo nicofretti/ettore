52a4583aa8fbdeec414a3803e2f3783f400b3142
Ledjo Lleshaj <ledjolleshaj@yahoo.com>
2023-01-10 16:31:01 +0100

    Added RegistrationPage object with web elements/getters/setters

--- src/test/java/it/ettore/e2e/PageObjects/PageObject.java
+++ src/test/java/it/ettore/e2e/PageObjects/PageObject.java
@@ -1,7 +1,6 @@
 package it.ettore.e2e.PageObjects;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 
 public class PageObject {
