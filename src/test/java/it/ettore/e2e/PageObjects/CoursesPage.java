3ae94cd3576c835b7b45448c08e414261b4f275c
Ledjo Lleshaj <ledjolleshaj@yahoo.com>
2023-01-10 18:14:07 +0100

    Init Courses Page Object

---
+++ src/test/java/it/ettore/e2e/PageObjects/CoursesPage.java
@@ -0,0 +1,13 @@
+package it.ettore.e2e.PageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.WebElement;
+
+public class CoursesPage extends PageObject{
+
+    public CoursesPage(WebDriver driver) {
+        super(driver);
+    }
+
+}
