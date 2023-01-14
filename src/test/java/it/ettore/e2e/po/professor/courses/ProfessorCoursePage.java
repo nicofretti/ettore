05392106afa6fa1540939ab04e1ca3152d06ae00
Ledjo Lleshaj <ledjolleshaj@yahoo.com>
2023-01-14 01:39:19 +0100

    Completed Lesson PO's

--- src/test/java/it/ettore/e2e/po/professor/courses/ProfessorCoursePage.java
+++ src/test/java/it/ettore/e2e/po/professor/courses/ProfessorCoursePage.java
@@ -2,6 +2,7 @@ package it.ettore.e2e.po.professor.courses;
 import it.ettore.e2e.po.Header;
 import it.ettore.e2e.po.LoginPage;
 import it.ettore.e2e.po.PageObject;
+import it.ettore.e2e.po.professor.ProfessorManagePage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
--- src/test/java/it/ettore/e2e/po/professor/lessons/ProfessorLessonsPage.java
+++ src/test/java/it/ettore/e2e/po/professor/lessons/ProfessorLessonsPage.java
@@ -2,7 +2,6 @@ package it.ettore.e2e.po.professor.lessons;
 
 import it.ettore.e2e.po.Header;
 import it.ettore.e2e.po.PageObject;
-import it.ettore.e2e.po.professor.courses.ProfessorCoursePage;
 import lombok.EqualsAndHashCode;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -28,6 +27,37 @@ public class ProfessorLessonsPage extends PageObject {
     public Object newCourse() {
         addNewLesson.click();
         return null;
+    }
+
+    @EqualsAndHashCode
+    public static class LessonComponent {
+        private WebDriver driver;
+        private WebElement element;
+
+        public LessonComponent(WebDriver driver, WebElement element) {
+            this.driver = driver;
+            this.element = element;
+        }
+
+        public String getTitle() {
+            return element.findElement(By.className("et-name")).getText();
+        }
+
+        public String getContent() {
+            return element.findElement(By.className("et-description")).getText();
+        }
+
+        public ProfessorLessonPage goTo() {
+            element.findElement(By.className("et-name")).click();
+            return new ProfessorLessonPage(driver);
+        }
+    }
+
+    @FindBy(css = ".et-content > div")
+    private List<WebElement> lessons;
+
+    public List<LessonComponent> getCourses() {
+        return lessons.stream().map(element -> new LessonComponent(driver, element)).collect(Collectors.toList());
     }
 
 }
