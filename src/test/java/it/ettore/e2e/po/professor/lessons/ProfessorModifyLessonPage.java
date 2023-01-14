61725167dd59eebe9122442eee9f3cd25cd2c982
Ledjo Lleshaj <ledjolleshaj@yahoo.com>
2023-01-14 21:23:29 +0100

    Modify lesson object finished

---
+++ src/test/java/it/ettore/e2e/po/professor/lessons/ProfessorModifyLessonPage.java
@@ -0,0 +1,57 @@
+package it.ettore.e2e.po.professor.lessons;
+
+import it.ettore.e2e.po.Header;
+import it.ettore.e2e.po.PageObject;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class ProfessorModifyLessonPage extends PageObject {
+    public ProfessorModifyLessonPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public Header headerComponent() {
+        return new Header(driver);
+    }
+
+    @FindBy(name = "name")
+    private WebElement title;
+
+    @FindBy(name = "description")
+    private WebElement description;
+
+    @FindBy(name = "content")
+    private WebElement content;
+
+    @FindBy(id = "btn-save-lesson")
+    private WebElement saveButton;
+
+    @FindBy(id = "btn-cancel-lesson")
+    private WebElement cancelButton;
+
+    public void setTitle(String title) {
+        this.title.clear();
+        this.title.sendKeys(title);
+    }
+
+    public void setDescription(String description) {
+        this.description.clear();
+        this.description.sendKeys(description);
+    }
+
+    public void setContent(String content) {
+        this.content.clear();
+        this.content.sendKeys(content);
+    }
+
+    public ProfessorLessonsPage saveLesson() {
+        saveButton.click();
+        return new ProfessorLessonsPage(driver);
+    }
+
+    public ProfessorLessonsPage cancelLesson() {
+        cancelButton.click();
+        return new ProfessorLessonsPage(driver);
+    }
+}
