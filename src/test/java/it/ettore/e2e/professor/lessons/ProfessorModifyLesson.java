532cc6efa4a047c9bfc61257f9ab5ea6ef74d6de
Ledjo Lleshaj <ledjolleshaj@yahoo.com>
2023-01-15 14:17:35 +0100

    Refactored e2e Tests

--- src/test/java/it/ettore/e2e/professor/lessons/ProfessorModifyLesson.java
+++ src/test/java/it/ettore/e2e/professor/lessons/ProfessorModifyLesson.java
@@ -9,6 +9,7 @@ import it.ettore.e2e.po.professor.lessons.ProfessorLessonsPage;
 import it.ettore.e2e.po.professor.lessons.ProfessorModifyLessonPage;
 import it.ettore.model.*;
 import it.ettore.utils.Breadcrumb;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -25,23 +26,29 @@ public class ProfessorModifyLesson extends E2EBaseTest {
     protected LessonRepository repoLesson;
 
     /*Tests all the breadcrumb links from courses page to specific lesson contents page*/
+    User professor;
+    Course course;
+    Lesson lesson;
+    Lesson lesson2;
+    ProfessorCoursesPage coursesPage;
+    ProfessorCoursePage courseDetails;
+    ProfessorLessonsPage lessonsPage;
 
-    /*Tests all the breadcrumb links when editing a course are correct*/
-    @Test
-    public void breadcrumbsWhenEditingLesson() {
+    @Before
+    public void prepareLessonModifierTests() {
         String email = "some.professor@ettore.it";
         String password = "SomeSecurePassword";
-        User professor = new User("Some", "Professor", email, password, User.Role.PROFESSOR);
+        professor = new User("Some", "Professor", email, password, User.Role.PROFESSOR);
         repoUser.save(professor);
 
-        Course course = new Course("Course name", "Course description", 2023, Course.Category.Maths, professor);
+        course = new Course("Course name", "Course description", 2023, Course.Category.Maths, professor);
         repoCourse.save(course);
         // Link the course to the professor
         professor.getCoursesTaught().add(course);
         repoUser.save(professor);
 
-        Lesson lesson = new Lesson("Lesson name", "Lesson description", "Lesson content", course);
-        Lesson lesson2 = new Lesson("Lesson name 2", "Lesson description 2", "Lesson content 2", course);
+        lesson = new Lesson("Lesson name", "Lesson description", "Lesson content", course);
+        lesson2 = new Lesson("Lesson name 2", "Lesson description 2", "Lesson content 2", course);
         repoLesson.saveAll(List.of(lesson, lesson2));
 
         course.getLessons().addAll(List.of(lesson, lesson2));
@@ -53,24 +60,18 @@ public class ProfessorModifyLesson extends E2EBaseTest {
         loginPage.setEmail(email);
         loginPage.setPassword(password);
 
-        ProfessorCoursesPage coursesPage = loginPage.loginAsProfessor();
-        assertEquals(List.of(
-                new Breadcrumb("COURSES", "/professor/courses")
-        ), coursesPage.headerComponent().getBreadcrumbs());
+        coursesPage = loginPage.loginAsProfessor();
 
         //click on course
-        ProfessorCoursePage courseDetails = coursesPage.getCourses().get(0).goTo();
-
-        // Should be in the details page for the course
-        assertEquals(String.format("/professor/courses/%d", course.getId()), currentPath());
-
-        assertEquals(List.of(
-                new Breadcrumb("COURSES", "/professor/courses"),
-                new Breadcrumb("COURSE NAME", String.format("/professor/courses/%d", course.getId()))
-        ), courseDetails.headerComponent().getBreadcrumbs());
-
+        courseDetails = coursesPage.getCourses().get(0).goTo();
         //click on the first lesson
-        ProfessorLessonsPage lessonsPage = courseDetails.goToLessons();
+        lessonsPage = courseDetails.goToLessons();
+    }
+
+
+    /*Tests all the breadcrumb links when editing a course are correct*/
+    @Test
+    public void breadcrumbsWhenEditingLesson() {
         assertEquals(List.of(
                 new Breadcrumb("COURSES", "/professor/courses"),
                 new Breadcrumb("COURSE NAME", String.format("/professor/courses/%d", course.getId())),
@@ -98,83 +99,27 @@ public class ProfessorModifyLesson extends E2EBaseTest {
     /*Tests all the breadcrumb links when adding a new course */
     @Test
     public void breadcrumbsWhenNewLesson() {
-        String email = "some.professor@ettore.it";
-        String password = "SomeSecurePassword";
-        User professor = new User("Some", "Professor", email, password, User.Role.PROFESSOR);
-        repoUser.save(professor);
-
-        Course course = new Course("Course name", "Course description", 2023, Course.Category.Maths, professor);
-        repoCourse.save(course);
-        // Link the course to the professor
-        professor.getCoursesTaught().add(course);
-        repoUser.save(professor);
-
-        driver.get(baseDomain() + "login");
-        LoginPage loginPage = new LoginPage(driver);
-
-        loginPage.setEmail(email);
-        loginPage.setPassword(password);
-
-        ProfessorCoursesPage coursesPage = loginPage.loginAsProfessor();
-        assertEquals(List.of(
-                new Breadcrumb("COURSES", "/professor/courses")
-        ), coursesPage.headerComponent().getBreadcrumbs());
-
-        //click on course
-        ProfessorCoursePage courseDetails = coursesPage.getCourses().get(0).goTo();
-
-        // Should be in the details page for the course
-        assertEquals(String.format("/professor/courses/%d", course.getId()), currentPath());
-
-        assertEquals(List.of(
-                new Breadcrumb("COURSES", "/professor/courses"),
-                new Breadcrumb("COURSE NAME", String.format("/professor/courses/%d", course.getId()))
-        ), courseDetails.headerComponent().getBreadcrumbs());
-
-        //click on the edit button
-        ProfessorModifyLessonPage editLesson = courseDetails.editLesson();
-        assertEquals(String.format("/professor/courses/%d/edit", course.getId()), currentPath());
-
-        //check the breadcrumbs
         assertEquals(List.of(
                 new Breadcrumb("COURSES", "/professor/courses"),
                 new Breadcrumb("COURSE NAME", String.format("/professor/courses/%d", course.getId())),
-                new Breadcrumb("EDIT", String.format("/professor/courses/%d/edit", course.getId()))
-        ), editLesson.headerComponent().getBreadcrumbs());
+                new Breadcrumb("LESSONS", String.format("/professor/courses/%d/lessons", course.getId()))
+        ), lessonsPage.headerComponent().getBreadcrumbs());
+
+        ProfessorModifyLessonPage modifyLessonPage = lessonsPage.newLesson();
+        assertEquals(List.of(
+                new Breadcrumb("COURSES", "/professor/courses"),
+                new Breadcrumb("COURSE NAME", String.format("/professor/courses/%d", course.getId())),
+                new Breadcrumb("LESSONS", String.format("/professor/courses/%d/lessons", course.getId())),
+                new Breadcrumb("ADD", String.format("/professor/courses/%d/lessons/add", course.getId()))
+        ), modifyLessonPage.headerComponent().getBreadcrumbs());
     }
 
 
     /*Tests the creation of a new lesson*/
     @Test
     public void addNewLesson() {
-        String email = "some.professor@ettore.it";
-        String password = "SomeSecurePassword";
-        User professor = new User("Some", "Professor", email, password, User.Role.PROFESSOR);
-        repoUser.save(professor);
-
-        Course course = new Course("Course name", "Course description", 2023, Course.Category.Maths, professor);
-        repoCourse.save(course);
-        // Link the course to the professor
-        professor.getCoursesTaught().add(course);
-        repoUser.save(professor);
-
-        driver.get(baseDomain() + "login");
-        LoginPage loginPage = new LoginPage(driver);
-
-        loginPage.setEmail(email);
-        loginPage.setPassword(password);
-
-        ProfessorCoursesPage coursesPage = loginPage.loginAsProfessor();
-        assertEquals("/professor/courses", currentPath());
-
-        //click on first course
-        ProfessorCoursePage courseDetails = coursesPage.getCourses().get(0).goTo();
-        assertEquals(String.format("/professor/courses/%d", professor.getCoursesTaught().get(0).getId()), currentPath());
-
-        //click lessons
-        ProfessorLessonsPage lessonsPage = courseDetails.goToLessons();
         assertEquals(String.format("/professor/courses/%d/lessons", professor.getCoursesTaught().get(0).getId()), currentPath());
-        assertEquals(0, lessonsPage.getLessons().size());
+        assertEquals(0, lessonsPage.getLessons().size()-2);
 
         //click on add new lesson
         ProfessorModifyLessonPage modifyLessonPage = lessonsPage.newLesson();
@@ -187,40 +132,14 @@ public class ProfessorModifyLesson extends E2EBaseTest {
 
         //click on save
         ProfessorLessonsPage lessonsPage2 = modifyLessonPage.saveLesson();
-        assertEquals(1, lessonsPage2.getLessons().size());
+        assertEquals(1, lessonsPage2.getLessons().size()-2);
     }
 
     /* Test modifying existing course */
     @Test
     public void modifyCourse() {
-        String email = "some.professor@ettore.it";
-        String password = "SomeSecurePassword";
-        User professor = new User("Some", "Professor", email, password, User.Role.PROFESSOR);
-        repoUser.save(professor);
-
-        Course course = new Course("Course name", "Course description", 2023, Course.Category.Maths, professor);
-        repoCourse.save(course);
-        // Link the course to the professor
-        professor.getCoursesTaught().add(course);
-        repoUser.save(professor);
-
-        driver.get(baseDomain() + "login");
-        LoginPage loginPage = new LoginPage(driver);
-
-        loginPage.setEmail(email);
-        loginPage.setPassword(password);
-
-        ProfessorCoursesPage coursesPage = loginPage.loginAsProfessor();
-        assertEquals("/professor/courses", currentPath());
-
-        //click on first course
-        ProfessorCoursePage courseDetails = coursesPage.getCourses().get(0).goTo();
-        assertEquals(String.format("/professor/courses/%d", professor.getCoursesTaught().get(0).getId()), currentPath());
-
-        //click lessons
-        ProfessorLessonsPage lessonsPage = courseDetails.goToLessons();
         assertEquals(String.format("/professor/courses/%d/lessons", professor.getCoursesTaught().get(0).getId()), currentPath());
-        assertEquals(0, lessonsPage.getLessons().size());
+        assertEquals(0, lessonsPage.getLessons().size()-2);
 
         //click on add new lesson
         ProfessorModifyLessonPage modifyLessonPage = lessonsPage.newLesson();
@@ -233,7 +152,7 @@ public class ProfessorModifyLesson extends E2EBaseTest {
 
         //click on save
         ProfessorLessonsPage lessonsPage2 = modifyLessonPage.saveLesson();
-        assertEquals(1, lessonsPage2.getLessons().size());
+        assertEquals(1, lessonsPage2.getLessons().size()-2);
 
         //click on lesson
         ProfessorLessonPage lessonPage = lessonsPage2.getLessons().get(0).goTo();
@@ -241,54 +160,22 @@ public class ProfessorModifyLesson extends E2EBaseTest {
         //click on edit
         ProfessorModifyLessonPage modifyLessonPage2 = lessonPage.editLesson();
 
-        //fill the form
-        modifyLessonPage2.setTitle("Lesson name 2");
-        modifyLessonPage2.setDescription("Lesson description 2");
+        //fill the formLesson
+        modifyLessonPage2.setTitle("New name 2");
+        modifyLessonPage2.setDescription("New Lesson description 2");
         modifyLessonPage2.setContent("# Lesson content 2");
 
         //click on save
-        ProfessorLessonsPage lessonPage2 = modifyLessonPage2.saveLesson();
-        //assertEquals(1, lessonPage2.getLessons().size());
+        lessonsPage2 = modifyLessonPage2.saveLesson();
 
-
+        assertEquals(1, lessonsPage2.getLessons().size()-2);
     }
 
     /* Test deleting existing lesson */
     @Test
     public void deleteLesson() {
-        String email = "some.professor@ettore.it";
-        String password = "SomeSecurePassword";
-        User professor = new User("Some", "Professor", email, password, User.Role.PROFESSOR);
-        repoUser.save(professor);
-
-        Course course = new Course("Course name", "Course description", 2023, Course.Category.Maths, professor);
-        repoCourse.save(course);
-
-        Lesson lesson = new Lesson("Lesson name", "Lesson description", "# Lesson content", course);
-        repoLesson.save(lesson);
-
-        course.getLessons().add(lesson);
-        repoCourse.save(course);
-
-        // Link the course to the professor
-        professor.getCoursesTaught().add(course);
-        repoUser.save(professor);
-
-        driver.get(baseDomain() + "login");
-        LoginPage loginPage = new LoginPage(driver);
-
-        loginPage.setEmail(email);
-        loginPage.setPassword(password);
-
-        ProfessorCoursesPage coursesPage = loginPage.loginAsProfessor();
-        assertEquals("/professor/courses", currentPath());
-
-        ProfessorCoursePage courseDetails = coursesPage.getCourses().get(0).goTo();
-        assertEquals(String.format("/professor/courses/%d", course.getId()), currentPath());
-
-        ProfessorLessonsPage lessonsPage = courseDetails.goToLessons();
         assertEquals(String.format("/professor/courses/%d/lessons", course.getId()), currentPath());
-        assertEquals(1, lessonsPage.getLessons().size());
+        assertEquals(course.getLessons().size(), lessonsPage.getLessons().size());
 
         ProfessorLessonPage lessonPage = lessonsPage.getLessons().get(0).goTo();
         assertEquals(String.format("/professor/courses/%d/lessons/%d", course.getId(), lesson.getId()), currentPath());
@@ -297,7 +184,7 @@ public class ProfessorModifyLesson extends E2EBaseTest {
 
         lessonsPage = modifyLessonPage.cancelLesson();
         assertEquals(String.format("/professor/courses/%d/lessons", course.getId()), currentPath());
-        assertEquals(0, lessonsPage.getLessons().size());
+        assertEquals(course.getLessons().size()-1, lessonsPage.getLessons().size());
     }
 
 }
