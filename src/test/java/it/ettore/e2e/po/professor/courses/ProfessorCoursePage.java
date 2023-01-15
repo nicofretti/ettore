d47d0bca0da0d5f6f62081bdaa333cb0e5fdc1b4
Nicolò Fretti <34170904+nicofretti@users.noreply.github.com>
2023-01-15 15:32:10 +0100

    Fix merge problems

--- src/test/java/it/ettore/e2e/po/professor/courses/ProfessorCoursePage.java
+++ src/test/java/it/ettore/e2e/po/professor/courses/ProfessorCoursePage.java
@@ -1,15 +1,27 @@
 package it.ettore.e2e.po.professor.courses;
+
 import it.ettore.e2e.po.Header;
-import it.ettore.e2e.po.LoginPage;
 import it.ettore.e2e.po.PageObject;
 import it.ettore.e2e.po.professor.ProfessorManagePage;
 import it.ettore.e2e.po.professor.lessons.ProfessorLessonsPage;
-import it.ettore.e2e.po.professor.lessons.ProfessorModifyLessonPage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class ProfessorCoursePage extends PageObject {
+    @FindBy(className = "et-name")
+    private WebElement name;
+    @FindBy(className = "et-period")
+    private WebElement period;
+    @FindBy(className = "et-description")
+    private WebElement description;
+    @FindBy(id = "btn-edit")
+    private WebElement editButton;
+    @FindBy(id = "btn-goto-manage")
+    private WebElement gotoManageButton;
+    @FindBy(id = "btn-goto-lessons")
+    private WebElement gotoLessonsButton;
+
     public ProfessorCoursePage(WebDriver driver) {
         super(driver);
     }
@@ -17,24 +29,6 @@ public class ProfessorCoursePage extends PageObject {
     public Header headerComponent() {
         return new Header(driver);
     }
-
-    @FindBy(className = "et-name")
-    private WebElement name;
-
-    @FindBy(className = "et-period")
-    private WebElement period;
-
-    @FindBy(className = "et-description")
-    private WebElement description;
-
-    @FindBy(id = "btn-edit")
-    private WebElement editButton;
-
-    @FindBy(id = "btn-goto-manage")
-    private WebElement gotoManageButton;
-
-    @FindBy(id = "btn-goto-lessons")
-    private WebElement gotoLessonsButton;
 
     public String getName() {
         return name.getText();
@@ -57,10 +51,10 @@ public class ProfessorCoursePage extends PageObject {
         gotoLessonsButton.click();
         return new ProfessorLessonsPage(driver);
     }
-    
-    public ProfessorModifyLessonPage editLesson() {
+
+    public ProfessorCourseAddPage editCourse() {
         editButton.click();
-        return new ProfessorModifyLessonPage(driver);
+        return new ProfessorCourseAddPage(driver);
     }
 }
 
