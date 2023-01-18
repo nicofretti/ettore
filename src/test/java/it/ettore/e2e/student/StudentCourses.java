66226c62bb0710a99cf09ce05e12a653b25f701e
Elia Perantoni <perantonielia0@gmail.com>
2023-01-18 16:05:17 +0100

    More coverage

--- src/test/java/it/ettore/e2e/student/StudentCourses.java
+++ src/test/java/it/ettore/e2e/student/StudentCourses.java
@@ -1,9 +1,11 @@
 package it.ettore.e2e.student;
 
 import it.ettore.e2e.E2EBaseTest;
+import it.ettore.e2e.po.ErrorsComponent;
 import it.ettore.e2e.po.LoginPage;
 import it.ettore.e2e.po.student.StudentCoursePage;
 import it.ettore.e2e.po.student.StudentCoursesPage;
+import it.ettore.e2e.po.student.StudentSearchPage;
 import it.ettore.model.Course;
 import it.ettore.model.CourseRepository;
 import it.ettore.model.User;
@@ -13,6 +15,7 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
+import java.util.Set;
 
 import static it.ettore.TestUtil.assertEmpty;
 import static org.junit.Assert.assertEquals;
@@ -210,5 +213,165 @@ public class StudentCourses extends E2EBaseTest {
         assertEquals(String.format("/student/courses", course.getId()), currentPath());
 
         assertEmpty(coursesPage.getCourses());
+    }
+
+    /**
+     * Check that the application doesn't allow a student to navigate or interact with a course that doesn't exist
+     */
+    @Test
+    public void cannotInteractWithNonExistingCourse() {
+        String email = "some.student@ettore.it";
+        String password = "SomeSecurePassword";
+        User student = new User("Some", "Student", email, password, User.Role.STUDENT);
+        repoUser.save(student);
+
+        driver.get(baseDomain() + "login");
+        LoginPage loginPage = new LoginPage(driver);
+
+        loginPage.setEmail(email);
+        loginPage.setPassword(password);
+
+        loginPage.loginAsStudent();
+
+        // Try to go to the details of a course that doesn't exist
+        driver.get(baseDomain() + "student/courses/420");
+        // Check that we're redirected to the list of courses
+        assertEquals("/student/courses", currentPath());
+
+        // Try to unsubscribe from a course that doesn't exist
+        driver.get(baseDomain() + "student/courses/420/unjoin");
+        // Check that we're redirected to the list of courses
+        assertEquals("/student/courses", currentPath());
+
+        // Try to request to join a course that doesn't exist
+        driver.get(baseDomain() + "student/courses/420/request-to-join");
+        // Check that we're redirected to the list of courses
+        assertEquals("/student/courses", currentPath());
+    }
+
+    /**
+     * Check that the application doesn't allow a student to navigate or interact with a course that he/she isn't
+     * enrolled in
+     */
+    @Test
+    public void cannotInteractWithNonJoinedCourse() {
+        String email = "some.student@ettore.it";
+        String password = "SomeSecurePassword";
+        User student = new User("Some", "Student", email, password, User.Role.STUDENT);
+        repoUser.save(student);
+
+        User professor = new User("Some", "Professor", "some.professor@ettore.it", "SomeSecurePassword", User.Role.PROFESSOR);
+        repoUser.save(professor);
+        Course course = new Course("Some course", "Description", 2022, Course.Category.Science, professor);
+        repoCourse.save(course);
+        professor.getCoursesTaught().add(course);
+        repoUser.save(professor);
+
+        driver.get(baseDomain() + "login");
+        LoginPage loginPage = new LoginPage(driver);
+
+        loginPage.setEmail(email);
+        loginPage.setPassword(password);
+
+        loginPage.loginAsStudent();
+
+        // Try to go to the details of a course that the student isn't enrolled in
+        driver.get(baseDomain() + "student/courses/" + course.getId());
+        // Check that we're redirected to the list of courses
+        assertEquals("/student/courses", currentPath());
+
+        // Try to unsubscribe from a course that the student isn't enrolled in
+        driver.get(baseDomain() + "student/courses/" + course.getId() + "/unjoin");
+        // Check that we're redirected to the list of courses
+        assertEquals("/student/courses", currentPath());
+    }
+
+    /**
+     * Check that the application doesn't allow a student to request to join a course that he/she has already requested
+     * to join
+     */
+    @Test
+    public void cannotRequestToJoinCourseAlreadyRequested() {
+        String email = "some.student@ettore.it";
+        String password = "SomeSecurePassword";
+        User student = new User("Some", "Student", email, password, User.Role.STUDENT);
+        repoUser.save(student);
+
+        User professor = new User("Some", "Professor", "some.professor@ettore.it", "SomeSecurePassword", User.Role.PROFESSOR);
+        repoUser.save(professor);
+        Course course = new Course("Some course", "Description", 2022, Course.Category.Science, professor);
+        course.requestJoin(student);
+        repoCourse.save(course);
+        professor.getCoursesTaught().add(course);
+        repoUser.save(professor);
+
+        driver.get(baseDomain() + "login");
+        LoginPage loginPage = new LoginPage(driver);
+
+        loginPage.setEmail(email);
+        loginPage.setPassword(password);
+
+        loginPage.loginAsStudent();
+
+        driver.get(baseDomain() + "student/courses/" + course.getId() + "/request-to-join");
+        // Check that we're redirected to the list of courses
+        assertEquals("/student/courses", currentPath());
+    }
+
+    /**
+     * Check that the application doesn't allow a student to request to join a course that he/she is already enrolled in
+     */
+    @Test
+    public void cannotRequestToJoinCourseAlreadyJoined() {
+        String email = "some.student@ettore.it";
+        String password = "SomeSecurePassword";
+        User student = new User("Some", "Student", email, password, User.Role.STUDENT);
+        repoUser.save(student);
+
+        User professor = new User("Some", "Professor", "some.professor@ettore.it", "SomeSecurePassword", User.Role.PROFESSOR);
+        repoUser.save(professor);
+        Course course = new Course("Some course", "Description", 2022, Course.Category.Science, professor);
+        course.requestJoin(student);
+        course.acceptStudent(student);
+        repoCourse.save(course);
+        professor.getCoursesTaught().add(course);
+        repoUser.save(professor);
+
+        driver.get(baseDomain() + "login");
+        LoginPage loginPage = new LoginPage(driver);
+
+        loginPage.setEmail(email);
+        loginPage.setPassword(password);
+
+        loginPage.loginAsStudent();
+
+        driver.get(baseDomain() + "student/courses/" + course.getId() + "/request-to-join");
+        // Check that we're redirected to the list of courses
+        assertEquals("/student/courses", currentPath());
+    }
+
+    /**
+     * Check that searches with bad parameters correctly result in error messages being shown
+     */
+    @Test
+    public void badSearch() {
+        String email = "some.student@ettore.it";
+        String password = "SomeSecurePassword";
+        User student = new User("Some", "Student", email, password, User.Role.STUDENT);
+        repoUser.save(student);
+
+        driver.get(baseDomain() + "login");
+        LoginPage loginPage = new LoginPage(driver);
+
+        loginPage.setEmail(email);
+        loginPage.setPassword(password);
+
+        loginPage.loginAsStudent();
+
+        driver.get(baseDomain() + "student/courses/search?startingYear=x");
+        assertEquals(Set.of("Invalid starting year"), new ErrorsComponent(driver).getErrorMessageSet());
+
+        driver.get(baseDomain() + "student/courses/search?category=x");
+        assertEquals(Set.of("Invalid category"), new ErrorsComponent(driver).getErrorMessageSet());
     }
 }
